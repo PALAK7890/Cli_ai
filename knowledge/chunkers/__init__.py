170f33716cbb8b85bb3f5240299d85f1d6400b43
Palak <palakdahiya78@gmail.com>
2026-07-29 18:06:41 +0530

    Export all chunker implementations from package root

--- knowledge/chunkers/__init__.py
+++ knowledge/chunkers/__init__.py
@@ -4,5 +4,13 @@ Text chunking module.
 
 from knowledge.chunkers.base import BaseChunker, DocumentChunk
 from knowledge.chunkers.fixed_size import FixedSizeChunker
+from knowledge.chunkers.sentence import SentenceChunker
+from knowledge.chunkers.recursive import RecursiveChunker
 
-__all__ = ["BaseChunker", "DocumentChunk", "FixedSizeChunker"]
+__all__ = [
+    "BaseChunker",
+    "DocumentChunk",
+    "FixedSizeChunker",
+    "SentenceChunker",
+    "RecursiveChunker",
+]
